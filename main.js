f08363507dd37ab94c106cafc933c3bb86712760
emin <emin@duweisst.de>
2023-11-11 18:18:27 +0100

    [FEAUTURE-ADD]: cleaned up, musik page with styling

---
+++ main.js
@@ -0,0 +1,11 @@
+const musik = document.getElementById('musik');
+const dev = document.getElementById('dev');
+const goku = document.getElementById('goku');
+
+musik.addEventListener('click', () => {
+    window.open("/html/musik.html", "_blank");
+});
+
+goku.addEventListener('click', () => {
+    window.open('https://home-5014728919.app-ionos.space/', '_blank')
+})
