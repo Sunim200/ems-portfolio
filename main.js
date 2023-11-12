5324d557b3ca6f10da14089e86418e67ba15533d
emin <emin@duweisst.de>
2023-11-12 14:29:30 +0100

    feature-add ems coding site with routing

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const musik = document.getElementById('musik');
-const dev = document.getElementById('dev');
+const emsCode = document.getElementById('emsCode');
 const goku = document.getElementById('goku');
 
 musik.addEventListener('click', () => {
     window.open("/html/musik.html", "_blank");
-    console.log('jsworks');
 });
 
-goku.addEventListener('click', () => {
-    window.open('https://home-5014728919.app-ionos.space/', '_blank')
-})
+emsCode.addEventListener('click', () => {
+    window.open('/html/emsCode.html', "_blank");
+    console.log('click');
+});
